cb80dadf6a82ca7fc4384ac69b2d3950b6523c67
funkygao <funky.gao@gmail.com>
2015-04-28 10:26:43 +0800

    CAS is better than lock/unlock in perf

--- bench_test.go
+++ bench_test.go
@@ -2,12 +2,27 @@ package nano
 
 import (
 	"strings"
+	"sync/atomic"
 	"testing"
+	"time"
 )
 
 func BenchmarkStringIndex(b *testing.B) {
 	addr := "tcp://eth0;124.56.74.124:1990"
 	for i := 0; i < b.N; i++ {
 		strings.Index(addr, "://")
+	}
+}
+
+func BenchmarkMakeTimer(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mkTimer(time.Nanosecond)
+	}
+}
+
+func BenchmarkCAS(b *testing.B) {
+	var v int32
+	for i := 0; i < b.N; i++ {
+		atomic.CompareAndSwapInt32(&v, 0, 1)
 	}
 }
