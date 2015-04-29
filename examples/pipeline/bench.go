c04155a4e373593bb2d906e70461880f5666408f
funkygao <funky.gao@gmail.com>
2015-04-29 16:00:33 +0800

    benchmark throughput of push-pull: 300K qps on my Mac

---
+++ examples/pipeline/bench.go
@@ -0,0 +1,116 @@
+// benchmark throughput of push-pull
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"sync/atomic"
+
+	"github.com/funkygao/golib/dashboard"
+	"github.com/funkygao/golib/stress"
+	"github.com/funkygao/nano"
+	"github.com/funkygao/nano/protocol/pull"
+	"github.com/funkygao/nano/protocol/push"
+	"github.com/funkygao/nano/transport"
+	"github.com/pkg/browser"
+)
+
+var (
+	addr             = "tcp://127.0.0.1:1234"
+	bodySize         = 1000
+	dashboardRefresh = 10
+)
+
+type metricsRecv struct {
+	last  int
+	total int64
+}
+
+func (this *metricsRecv) Data() int {
+	total := int(atomic.LoadInt64(&this.total))
+	r := (total - this.last) / dashboardRefresh
+	this.last = total
+	return r
+}
+
+func init() {
+	nano.Debug = false
+}
+
+func usage() {
+	fmt.Printf("Usage: %s <push|pull>\n", os.Args[0])
+	os.Exit(0)
+}
+
+func runPull(seq int) {
+	db := dashboard.New("qps", dashboardRefresh)
+	g := db.AddGraph("qps")
+	metrics := &metricsRecv{}
+	g.AddLine("recv", metrics)
+	go db.Launch(":8000")
+	browser.OpenURL("http://localhost:8000/")
+
+	sock, err := pull.NewSocket()
+	if err != nil {
+		panic(err)
+	}
+
+	transport.AddAll(sock)
+	if err := sock.Listen(addr); err != nil {
+		panic(err)
+	}
+
+	for {
+		_, err := sock.Recv()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		atomic.AddInt64(&metrics.total, 1)
+	}
+
+	if err := sock.Close(); err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func runPush(seq int) {
+	sock, err := push.NewSocket()
+	if err != nil {
+		panic(err)
+	}
+
+	transport.AddAll(sock)
+	if err := sock.Dial(addr); err != nil {
+		panic(err)
+	}
+
+	body := []byte(strings.Repeat("X", bodySize))
+	for i := 0; i < 100000; i++ {
+		sock.Send(body)
+		stress.IncCounter("qps", 1)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "push":
+		stress.RunStress(runPush)
+
+	case "pull":
+		runPull(0)
+
+	default:
+		usage()
+	}
+
+	select {}
+
+}
