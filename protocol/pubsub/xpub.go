bc202b922b8bbec16f0e36485e42a220468d5c5c
funkygao <funky.gao@gmail.com>
2015-05-07 15:43:36 +0800

    skeleton of xpub/xsub

--- protocol/pubsub/xpub.go
+++ protocol/pubsub/xpub.go
@@ -1,1 +1,55 @@
 package pubsub
+
+import (
+	"time"
+
+	"github.com/funkygao/nano"
+)
+
+type xpub struct {
+	sock nano.ProtocolSocket
+}
+
+func (this *xpub) Init(sock nano.ProtocolSocket) {
+	this.sock = sock
+}
+
+func (this *xpub) AddEndpoint(ep nano.Endpoint) {
+
+}
+
+func (this *xpub) RemoveEndpoint(ep nano.Endpoint) {
+
+}
+
+func (this *xpub) Shutdown(expire time.Time) {
+
+}
+
+func (this *xpub) SetOption(name string, val interface{}) error {
+	return nil
+}
+
+func (this *xpub) GetOption(name string) (interface{}, error) {
+	return nil, nil
+}
+
+func (this *xpub) Name() string {
+	return "pub"
+}
+
+func (this *xpub) PeerName() string {
+	return "sub"
+}
+
+func (this *xpub) Number() uint16 {
+	return nano.ProtoPub
+}
+
+func (this *xpub) PeerNumber() uint16 {
+	return nano.ProtoSub
+}
+
+func MakeXPubSocket() nano.Socket {
+	return nano.MakeSocket(&xpub{})
+}
--- protocol/pubsub/xsub.go
+++ protocol/pubsub/xsub.go
@@ -1,1 +1,55 @@
 package pubsub
+
+import (
+	"time"
+
+	"github.com/funkygao/nano"
+)
+
+type xsub struct {
+	sock nano.ProtocolSocket
+}
+
+func (this *xsub) Init(sock nano.ProtocolSocket) {
+	this.sock = sock
+}
+
+func (this *xsub) AddEndpoint(ep nano.Endpoint) {
+
+}
+
+func (this *xsub) RemoveEndpoint(ep nano.Endpoint) {
+
+}
+
+func (this *xsub) Shutdown(expire time.Time) {
+
+}
+
+func (this *xsub) SetOption(name string, val interface{}) error {
+	return nil
+}
+
+func (this *xsub) GetOption(name string) (interface{}, error) {
+	return nil, nil
+}
+
+func (this *xsub) Name() string {
+	return "sub"
+}
+
+func (this *xsub) PeerName() string {
+	return "pub"
+}
+
+func (this *xsub) Number() uint16 {
+	return nano.ProtoSub
+}
+
+func (this *xsub) PeerNumber() uint16 {
+	return nano.ProtoPub
+}
+
+func NewXSubSocket() nano.Socket {
+	return nano.MakeSocket(&xsub{})
+}
