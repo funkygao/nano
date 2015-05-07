b0a3a3fe21b95f7d7d462d9bd41f004944984922
funkygao <funky.gao@gmail.com>
2015-05-07 10:28:37 +0800

    pubsub refactor

--- protocol/pubsub/pub.go
+++ protocol/pubsub/pub.go
@@ -9,17 +9,34 @@ import (
 
 type pubEp struct {
 	ep nano.Endpoint
-	q  chan *nano.Message
-	p  *pub
-	w  nano.Waiter
+
+	q chan *nano.Message
+	w nano.Waiter
 }
 
+func (pe *pubEp) peerSender() {
+	var msg *nano.Message
+	for {
+		msg = <-pe.q
+		if msg == nil {
+			break
+		}
+
+		if pe.ep.SendMsg(msg) != nil {
+			msg.Free()
+			break
+		}
+	}
+
+	pe.w.Done()
+}
+
+// pub will listen and accept subs.
 type pub struct {
-	sock nano.ProtocolSocket
-	eps  map[uint32]*pubEp
-	raw  bool
-	w    nano.Waiter
-	init sync.Once
+	sock   nano.ProtocolSocket
+	eps    map[uint32]*pubEp // endpoints
+	raw    bool
+	waiter nano.Waiter
 
 	sync.Mutex
 }
@@ -27,12 +44,72 @@ type pub struct {
 func (p *pub) Init(sock nano.ProtocolSocket) {
 	p.sock = sock
 	p.eps = make(map[uint32]*pubEp)
+
+	// send only
 	p.sock.SetRecvError(nano.ErrProtoOp)
-	p.w.Init()
+
+	p.waiter.Init()
+	p.waiter.Add()
+	go p.sender()
+}
+
+func (p *pub) AddEndpoint(ep nano.Endpoint) {
+	qlen := 16
+	if i, err := p.sock.GetOption(nano.OptionWriteQLen); err == nil {
+		qlen = i.(int)
+	}
+
+	subscriber := &pubEp{
+		ep: ep,
+		q:  make(chan *nano.Message, qlen),
+	}
+	p.Lock()
+	p.eps[ep.Id()] = subscriber
+	p.Unlock()
+
+	subscriber.w.Init()
+	subscriber.w.Add()
+	go subscriber.peerSender()
+	go nano.NullRecv(ep)
+}
+
+func (p *pub) RemoveEndpoint(ep nano.Endpoint) {
+	p.Lock()
+	delete(p.eps, ep.Id())
+	p.Unlock()
+}
+
+func (p *pub) sender() {
+	defer p.waiter.Done()
+
+	sendChan := p.sock.SendChannel()
+	closeChan := p.sock.CloseChannel()
+	var msg *nano.Message
+	for {
+		select {
+		case <-closeChan:
+			return
+
+		case msg = <-sendChan:
+			// copy message to each endpoints
+			p.Lock()
+			for _, peer := range p.eps {
+				m := msg.Dup()
+				select {
+				case peer.q <- m:
+
+				default:
+					// sub queue full, silently drop
+					m.Free()
+				}
+			}
+			p.Unlock()
+		}
+	}
 }
 
 func (p *pub) Shutdown(expire time.Time) {
-	p.w.WaitAbsTimeout(expire)
+	p.waiter.WaitAbsTimeout(expire)
 
 	p.Lock()
 	peers := p.eps
@@ -44,76 +121,6 @@ func (p *pub) Shutdown(expire time.Time) {
 		close(peer.q)
 		delete(peers, id)
 	}
-}
-
-// Bottom sender.
-func (pe *pubEp) peerSender() {
-	for {
-		m := <-pe.q
-		if m == nil {
-			break
-		}
-
-		if pe.ep.SendMsg(m) != nil {
-			m.Free()
-			break
-		}
-	}
-}
-
-// Top sender.
-func (p *pub) sender() {
-	defer p.w.Done()
-
-	sq := p.sock.SendChannel()
-	cq := p.sock.CloseChannel()
-
-	for {
-		select {
-		case <-cq:
-			return
-
-		case m := <-sq:
-
-			// copy message to each endpoints
-			p.Lock()
-			for _, peer := range p.eps {
-				m := m.Dup()
-				select {
-				case peer.q <- m:
-				default:
-					m.Free()
-				}
-			}
-			p.Unlock()
-		}
-	}
-}
-
-func (p *pub) AddEndpoint(ep nano.Endpoint) {
-	p.init.Do(func() {
-		p.w.Add()
-		go p.sender()
-	})
-	depth := 16
-	if i, err := p.sock.GetOption(nano.OptionWriteQLen); err == nil {
-		depth = i.(int)
-	}
-	pe := &pubEp{ep: ep, p: p, q: make(chan *nano.Message, depth)}
-	pe.w.Init()
-	p.Lock()
-	p.eps[ep.Id()] = pe
-	p.Unlock()
-
-	pe.w.Add()
-	go pe.peerSender()
-	go nano.NullRecv(ep)
-}
-
-func (p *pub) RemoveEndpoint(ep nano.Endpoint) {
-	p.Lock()
-	delete(p.eps, ep.Id())
-	p.Unlock()
 }
 
 func (*pub) Number() uint16 {
@@ -140,6 +147,7 @@ func (p *pub) SetOption(name string, v interface{}) error {
 			return nano.ErrBadValue
 		}
 		return nil
+
 	default:
 		return nano.ErrBadOption
 	}
@@ -149,6 +157,7 @@ func (p *pub) GetOption(name string) (interface{}, error) {
 	switch name {
 	case nano.OptionRaw:
 		return p.raw, nil
+
 	default:
 		return nil, nano.ErrBadOption
 	}
