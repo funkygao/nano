335478e99ae1e08b42baf65a1d27e640a15aaf21
funkygao <funky.gao@gmail.com>
2015-05-11 08:10:45 +0800

    nil

--- apps/rpc/server.go
+++ apps/rpc/server.go
@@ -1,15 +1,47 @@
 package rpc
 
+import (
+	"bytes"
+
+	"github.com/funkygao/nano"
+	"github.com/funkygao/nano/protocol/reqrep"
+)
+
 type server struct {
+	sock nano.Socket
 }
 
 func (this *server) Register() {
 }
 
-func (this *server) Serv(addr string) {
+func (this *server) handleCall(buf *bytes.Buffer) error {
+	nano.BufferPoolPut(buf)
+	return nil
+}
+
+func (this *server) Serv(addr string) error {
+	var err error
+	if err = this.sock.Listen(addr); err != nil {
+		return err
+	}
+
+	for {
+		buf := nano.BufferPoolGet()
+		_, err = this.sock.XRecv(buf)
+		if err != nil {
+			return err
+		}
+
+		if err = this.handleCall(buf); err != nil {
+			return err
+		}
+
+	}
 }
 
 func NewServer() *server {
-	s := &server{}
+	s := &server{
+		sock: reqrep.NewRepSocket(),
+	}
 	return s
 }
