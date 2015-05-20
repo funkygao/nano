67e26d8698d02766d0e2caf3ab1c18131eb022b7
funkygao <funky.gao@gmail.com>
2015-05-20 08:06:28 +0800

    mq

--- protocol/mq/pub.go
+++ protocol/mq/pub.go
@@ -1,49 +1,116 @@
 package mq
 
 import (
+	"sync"
 	"time"
 
 	"github.com/funkygao/nano"
 )
 
+type pubEp struct {
+	ep      nano.Endpoint
+	msgChan chan *nano.Message
+}
+
+func (this *pubEp) sendToBroker() {
+	var msg *nano.Message
+	for msg = range this.msgChan {
+		if msg == nil {
+			break
+		}
+
+		if err := this.ep.SendMsg(msg); err != nil {
+			nano.Debugf(err.Error())
+			msg.Free()
+			break
+		}
+
+		msg.Free()
+	}
+
+}
+
 type pub struct {
-	sock nano.ProtocolSocket
+	sock    nano.ProtocolSocket
+	brokers map[nano.EndpointId]*pubEp
+	once    sync.Once
+	waiter  nano.Waiter
+	sync.Mutex
 }
 
 func (this *pub) Init(sock nano.ProtocolSocket) {
 	this.sock = sock
+	this.brokers = make(map[nano.EndpointId]*pubEp)
+	this.waiter.Init()
 }
 
 func (this *pub) AddEndpoint(ep nano.Endpoint) {
+	this.once.Do(func() {
+		this.waiter.Add()
+		go this.sender()
+	})
+
+	nano.Debugf("connected with %s", ep.RemoteAddr())
 	if err := handshake(ep); err != nil {
 		nano.Debugf(err.Error())
 		return
 	}
 
-	go this.sender(ep)
+	qlen := 16
+	/*
+		if i, err := this.sock.GetOption(nano.OptionWriteQLen); err == nil {
+			//qlen = i.(int)
+		}*/
+	this.Lock()
+	b := &pubEp{
+		ep:      ep,
+		msgChan: make(chan *nano.Message, qlen),
+	}
+	this.brokers[ep.Id()] = b
+	this.Unlock()
 
+	this.waiter.Add()
+	go b.sendToBroker()
+	go nano.NullRecv(ep)
 }
 
-func (this *pub) sender(ep nano.Endpoint) {
+// top sender
+func (this *pub) sender() {
+	defer this.waiter.Done()
+
 	sendChan := this.sock.SendChannel()
 	closeChan := this.sock.CloseChannel()
-
+	var msg *nano.Message
 	for {
 		select {
 		case <-closeChan:
 			return
 
-		case msg := <-sendChan:
-			if err := ep.SendMsg(msg); err != nil {
-				return
+		case msg = <-sendChan:
+			this.Lock()
+			for _, b := range this.brokers {
+				m := msg.Dup()
+				select {
+				case b.msgChan <- m:
+
+				default:
+					m.Free()
+
+				}
+				b.msgChan <- msg
 			}
-			ep.Flush()
+			this.Unlock()
+			msg.Free()
 		}
 	}
 }
 
 func (this *pub) RemoveEndpoint(ep nano.Endpoint) {
-
+	this.Lock()
+	b := this.brokers[ep.Id()]
+	close(b.msgChan)
+	delete(this.brokers, ep.Id())
+	this.Unlock()
 }
 
 func (this *pub) Shutdown(expire time.Time) {
